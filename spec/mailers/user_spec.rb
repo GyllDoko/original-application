cbeeb214178b32590666a0a756bafdfa896adc4d
DOKO ALFA Gyll-christ <dokogyll@gmail.com>
2021-10-22 16:53:02 +0100

    aohond

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "welcome_mail" do
-    let(:mail) { UserMailer.welcome_mail }
+    user = User.create!(email: Faker::Internet.email, password: "passwoioj")
+    let(:mail) { UserMailer.welcome_mail(user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Welcome mail")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).not_to eq("Welcome mail")
+      expect(mail.to).not_to eq(["to@example.org"])
+      expect(mail.from).to eq(["dokogyll@gmail.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).not_to match("Hi")
     end
   end
 
